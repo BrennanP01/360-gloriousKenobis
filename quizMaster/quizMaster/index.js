504fa88e47338a5d7ba8d2429eee9c55653de59b
BrennanP01 <brennanjprice@lewisu.edu>
2021-03-14 17:42:26 -0500

    added the info grab function to the server, with json question files

--- quizMaster/quizMaster/index.js
+++ quizMaster/quizMaster/index.js
@@ -1,30 +1,57 @@
-const http = require('http');
-const { Recoverable } = require('repl');
-const url = require('url');
+const { info } = require('console');
+const fs = require('fs');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    
-    let idList = []
-    if(req.query.id){
-        idList = req.query.id.split(",");
-    } else {
-        idList.push("Bob");
+    const goal = req.query.goal;
+    var responseMessage = "";
+
+    let qbMasterListFile = fs.readFileSync(__dirname + '/qbMasterList.json');
+    let qbMasterList = JSON.parse(qbMasterListFile);
+    qbMasterList = qbMasterList.questionBanks;
+
+    for(x in qbMasterList){
+        console.log(x);
+        console.log(qbMasterList[x]);
     }
 
-    let amountList = []
-    if(req.query.amount){
-        amountList = req.query.amount.split(",");
-        for(var i=0; i < idList.length; i++){
-            amountList[i] = Number(amountList[i]);
+    if(goal == "info"){
+        console.log("info");
+        var questionBankInfo = {};
+        for(x in qbMasterList){
+            let questionBankRaw = fs.readFileSync(__dirname + "/" + qbMasterList[x]);
+            let questionBank = JSON.parse(questionBankRaw);
+            console.log(questionBank.Name + " " + questionBank.ID + " " +  questionBank.Number);
+            // for(j in questionBank){
+            //     console.log();
+            // }
         }
-    } else {
-        amountList.push(0);
-    }
+        // let qbList = []
+        // if(req.query.qb){
+        //     qbList = req.query.qb.split(",");
+        // }else{
+        //     qbList.push("appliedProgQuestions")
+        // }
+        // let idList = []
+        // if(req.query.id){
+        //     idList = req.query.id.split(",");
+        // } else {
+        //     idList.push("Bob");
+        // }
 
-    var responseMessage = ""
-    for(var i = 0; i < idList.length; i++){
-        responseMessage = responseMessage + "Question Bank " + idList[i] + " requests " + amountList[i] + " questions." + "\n";
+        // let amountList = []
+        // if(req.query.amount){
+        //     amountList = req.query.amount.split(",");
+        //     for(var i=0; i < idList.length; i++){
+        //         amountList[i] = Number(amountList[i]);
+        //     }
+        // } else {
+        //     amountList.push(0);
+        // }
+
+        // for(var i = 0; i < idList.length; i++){
+        //     responseMessage = responseMessage + "Question Bank " + idList[i] + " requests " + amountList[i] + " questions." + "\n";
+        // }
     }
     context.res = {
         // status: 200, /* Defaults to 200 */
