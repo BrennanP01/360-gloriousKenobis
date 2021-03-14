45938193e1a729cd4075cf8fea5e5169a7b8d336
BrennanP01 <brennanjprice@lewisu.edu>
2021-03-13 22:17:14 -0600

    made infestructure to get multiple queries from one url

--- quizMaster/quizMaster/index.js
+++ quizMaster/quizMaster/index.js
@@ -1,11 +1,29 @@
+const http = require('http');
+const { Recoverable } = require('repl');
+const url = require('url');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    
+    let nameList = []
+    if(req.query.name){
+        nameList = req.query.name.split(",");
+    } else {
+        nameList.push("Bob");
+    }
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    let ageList = []
+    if(req.query.age){
+        ageList = req.query.age.split(",");
+    } else {
+        ageList.push("15");
+    }
 
+    console.log(typeof nameList);
+    var responseMessage = ""
+    for(var i = 0; i < nameList.length; i++){
+        responseMessage = responseMessage + "Hello, " + nameList[i] + ". You are " + ageList[i] + " years old. This HTTP triggered function executed successfully." + "\n";
+    }
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
