5a166204fdd3eae4b4895151f01b9e7efa9c72af
BrennanP01 <brennanjprice@lewisu.edu>
2021-03-15 01:26:38 -0500

    improved comments

--- quizMaster/quizMaster/index.js
+++ quizMaster/quizMaster/index.js
@@ -2,7 +2,6 @@ const { info } = require('console');
 const fs = require('fs');
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
     const goal = req.query.goal;
     var responseMessage = "";
 
@@ -17,26 +16,23 @@ module.exports = async function (context, req) {
         questionBankInfo[questionBank.ID] = [questionBank.ID, questionBank.Name, questionBank.Number, qbMasterList[x]];
     }
 
-    if(goal == "info"){
-        console.log("info");
-        let responseInfo = JSON.parse(JSON.stringify(questionBankInfo));        
+    if(goal == "info"){ //if the user requests the information about the quizbanks
+        let responseInfo = JSON.parse(JSON.stringify(questionBankInfo)); //creates copy       
         for(id in responseInfo){
             entry = responseInfo[id];
-            entry.splice(3,1);
+            entry.splice(3,1); //remove dir path from info screen
         }
 
         responseMessage = JSON.stringify(responseInfo);
 
-
-
-    } else if (goal == "quiz") {
-        let idList = [];
+    } else if (goal == "quiz") {//if the user requests a quiz form
+        let idList = [];//gets the id's of all requested question banks
         if(req.query.id){
             idList = req.query.id.split(",");
         } else {
             idList.push("");
         }
-        let amountList = [];
+        let amountList = [];//gets the amount for all requested question banks
         if(req.query.amount){
             amountList = req.query.amount.split(",");
             for(var i = 0; i < amountList.length; i++){
@@ -47,12 +43,12 @@ module.exports = async function (context, req) {
         }
 
 
-        let quiz = [];
+        let quiz = [];//create an empty array to hold the quiz questions from each quizbank
         for(bank in questionBankInfo){
-            bankPath = questionBankInfo[bank][3];
-            qBank = JSON.parse(fs.readFileSync(__dirname + "/" + bankPath));
-            var index = idList.indexOf(qBank.ID);
-            let questionLocation = [];
+            bankPath = questionBankInfo[bank][3];//gets the relative path
+            qBank = JSON.parse(fs.readFileSync(__dirname + "/" + bankPath));//reads to object
+            var index = idList.indexOf(qBank.ID);//used to aquire which quiz to read from
+            let questionLocation = [];//randomize the order of questions to the amount that the user needs
             for(i = 0; i < amountList[index]; i++){
                 qLoc = Math.floor(Math.random() * Math.floor(questionBankInfo[bank][2]));
                 if(questionLocation.includes(qLoc)){
@@ -86,24 +82,24 @@ module.exports = async function (context, req) {
                         randomizedChoicesIndex.push(cLoc);
                     }
                 }
-                let randomizedChoices = [];
+                let randomizedChoices = [];//set the choices to the randomized order
                 for(i=0;i<randomizedChoicesIndex.length;i++){
                     randomizedChoices.push(randomQuestion["choices"][randomizedChoicesIndex[i]]);
                 }
-                
+                //replace choices with randomized ones
                 randomQuestion.choices = randomizedChoices;
 
                 
                 chosen.Questions.push(randomQuestion); //get the question and put it in chosen at the index
                 index++;
             }
-            quiz.push(chosen);
+            quiz.push(chosen);//puts the questions for each questionbank the user chose into an array to return
         }
-        responseMessage = JSON.stringify(quiz);
+        responseMessage = JSON.stringify(quiz); //sets the array of questions to be the response
     }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: responseMessage //set the body of the page to the json file of questions
     };
 }
