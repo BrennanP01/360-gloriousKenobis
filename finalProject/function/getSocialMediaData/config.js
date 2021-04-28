33c40801032f4694af4b17289c7c5565a0772989
BrennanP01 <brennanjprice@lewisu.edu>
2021-04-28 00:27:06 -0500

    working example of twitter API

---
+++ finalProject/function/getSocialMediaData/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:   'dg7vECvilDLsxwkQdFLUN3u5N',
+    consumer_secret:'bP9RgzEcOuki1qMawrpy3A74xHf5HQgSE6gIPUSX3ODMYfFkRR',
+    access_token:'750696863599251457-Fdq0GIroqkbWUv5JGNe4azvp4OMfQEq',
+    access_token_secret:'D5DYiisPeOGdXBh5VxEbEErT9u3DqTOP9xfp2EbYSr6r6'
+}
--- finalProject/function/getSocialMediaData/index.js
+++ finalProject/function/getSocialMediaData/index.js
@@ -1,9 +1,14 @@
 //https://sproutsocial.com/insights/best-times-to-post-on-social-media/
 // Source for best times to post on the different sites, based off of global engagement
 
+// package for twitter API
 var Twit = require('twit');
+var config = require('./config');
+//?plat=twitter&hash=rainbow&time=cst
+
 
 module.exports = async function (context, req) {
+    console.log("Starting");
     // inputs for the function
     const platform = req.query.plat;
     const hashtag = req.query.hash;
@@ -31,7 +36,7 @@ module.exports = async function (context, req) {
         responseMessage = "twitter";
         const twitterIdealTime = 9; // TODO find right time
         responseTime = calculateTime(twitterIdealTime, timezone);
-        //responseHashes = getTwitterHashes(hashtag);
+        responseHashes = getTwitterHashes(hashtag);
 
     }else{
         responseMessage = "Error: Unsupported Platform";
@@ -72,17 +77,70 @@ function calculateTime(time, timeZone){
     }
 }
 
+/***********************************
+            FACEBOOK
+***********************************/
 //gets related hashtags from facebook
 function getFacebookHashes(hashtag){
     posts = fetch()
 }
 
+/***********************************
+            INSTAGRAM
+***********************************/
 //gets related hashtags from instagram
 function getInstagramHashes(hashtag){
     
 }
 
+
+/***********************************
+            TWITTER
+***********************************/
 //gets related hashtags from twitter
 function getTwitterHashes(hashtag){
-    
+    var T = new Twit(config);
+    params = {
+        q: "#" + hashtag + " -is:retweet",
+        count: 50
+    }
+    console.log("Paramaters:")
+    console.log(params);
+    T.get('search/tweets', params, onDataRecieved);
+    return "yep"
+}
+
+function onDataRecieved(err, data, response){
+    var hashtagOccurances = []
+    var tweets = data.statuses;
+    for(var i = 0; i < tweets.length; i++){
+        var hashtags = tweets[i].entities.hashtags;
+        for(var j = 0; j < hashtags.length; j++){
+            console.log(hashtags[j]);
+            var inArrayLocation = null;
+            var isInArray = false;
+            for(let k = 0; k < hashtagOccurances.length; k++){
+                var compareValOne = hashtagOccurances[k].hash;
+                var compareValTwo = hashtags[j].text;
+                if(hashtagOccurances[k].hash == hashtags[j].text){
+                    isInArray = true;
+                    inArrayLocation = k;
+                }
+            }
+            if(isInArray){
+                let curVal = hashtagOccurances[inArrayLocation].occur
+                hashtagOccurances[inArrayLocation].occur = curVal + 1;
+                isInArray = false;
+            }else{
+                hashtagOccurances.push({
+                    hash: hashtags[j].text,
+                    occur: 1
+                });
+            }
+        }
+    }
+    hashtagOccurances.sort(function(a,b){
+        return b.occur - a.occur;
+    });
+    console.log(hashtagOccurances);
 }
